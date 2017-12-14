164b527586256afe61523c9726642030505b3290
iyuangang <yuangang@me.com>
2017-12-14 13:45:14 +0800

    remove in 5 min

--- prime3.py
+++ prime3.py
@@ -1,9 +1,9 @@
 import math
 
-def sushu(start,end):
+def prime(start,end):
     count=0
     for i in range(start,end+1):
-        if(i%2 == 0 and i != 2): 
+        if(i%2 == 0 and i != 2):
             continue
         for j in range(2,int(math.sqrt(i))+1):
             if(i%j==0):
@@ -15,8 +15,8 @@ def sushu(start,end):
     print('count',count)
     return
 
-#start=int(input("start:\n"))
-#end=int(input("end:\n"))
-#sushu(start,end)
+start=int(input("start:\n"))
+end=int(input("end:\n"))
+prime(start,end)
 
-sushu(1,1000)
+#prime(1,1000)
