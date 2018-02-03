3cc035f01f67497e0fe33bfdaaf96e698ef2abcf
iyuangang <yuangang@me.com>
2018-02-03 11:52:01 +0800

    python.py bug fix

--- python.py
+++ python.py
@@ -4,7 +4,7 @@ def drawSnake(rad, angle, len, neckrad):
     for i in range(len):
         turtle.circle(rad, angle)
         turtle.circle(-rad, angle)
-    turtle,circle(rad, angle/2)
+    turtle.circle(rad, angle/2)
     turtle.fd(rad)
     turtle.circle(neckrad+1, 180)
     turtle.fd(rad*2/3)
