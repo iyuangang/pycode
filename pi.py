a78d3983bff9f1178f14171890e74b0f9e50af84
iyuangang <yuangang@me.com>
2018-02-03 14:47:56 +0800

    improve pi.py

--- pi.py
+++ pi.py
@@ -2,14 +2,20 @@
 from random import random
 from math import sqrt
 from time import clock
-DARTS = 1000000
+DARTS = 100000000
 hits = 0
+j =0
 clock()
 for i in range(1,DARTS):
     x, y = random(), random()
     dist = sqrt(x**2+y**2)
     if dist<=1.0:
-        hits = hits +1
+        hits += 1
+        j += 1
+    if j==1000000:
+        j=0
+        t=4*hits/i
+        print("Pi now is: %s" %t) 
 pi = 4 * (hits/DARTS)
 print("Pi equals to: %s" %pi)
 print("program run time is %-5.5ss" % clock())
