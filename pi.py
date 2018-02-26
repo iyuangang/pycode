6d722b51978455e8e20d366f00370318856dbb1b
iyuangang <yuangang@me.com>
2018-02-26 10:31:20 +0800

    new py.py

--- pi.py
+++ pi.py
@@ -2,7 +2,7 @@
 from random import random
 from math import sqrt
 from time import clock
-DARTS = 100000000000000
+DARTS = 1000000000000
 hits = 0
 j =0
 clock()
@@ -16,6 +16,7 @@ for i in range(1,DARTS):
         j=0
         t=4*hits/i
         print("Pi now is: %s" %t) 
+        print("Time used: %-5.5ss" %clock())
 pi = 4 * (hits/DARTS)
 print("Pi equals to: %s" %pi)
 print("program run time is %-5.5ss" % clock())
