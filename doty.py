892d0dd9d96637dc8d63a084d2a15abd8c5f0029
Tim Yuan <timyuan@ubuntu.ttmai>
2018-10-05 23:52:54 +0800

    add doty bos

--- doty.py
+++ doty.py
@@ -6,14 +6,6 @@ month = int(input('Input month: '))
 day = int(input('Input day: '))
 total = 0
 tries = 100
-while tries > 0:
-    if month > 12 or month < 1:
-        print'Invaild input'
-        month = int(input('Input month: '))
-    elif day > 32 or day < 1:
-        print'Invaild input'
-        day = int(input('Input day: '))
-    tries = tries - 1
 if year%4 == 0:
     days = 29
     yee = 'leap'
@@ -32,6 +24,20 @@ if year%4 == 0:
 else:
     days = 28
     yee = 'common'
+while tries > 0:
+    if month > 12 or month < 1:
+        print'Invaild input'
+        month = int(input('Input month: '))
+    if (month in (1,3,5,7,8,10,12) and day > 32) or day < 1:
+        print'Invaild input'
+        day = int(input('Input day: '))
+    if (month in (2,4,6,9,11) and day > 31) or day < 1:
+        print'Invaild input'
+        day = int(input('Input day: '))
+    if month==2 and day>days:
+        print'Invaild input'
+        day = int(input('Input day: '))
+    tries = tries - 1
 print year, 'year is',yee,'Feb has', days, 'days'
 if month <= 1:
     total = day
