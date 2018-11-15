37ab592ce6bcdc0b3d1d05c694328f106b39ba73
Tim Yuan <timyuan@ubuntu.ttmai>
2018-11-15 16:57:26 +0800

    new code

--- invest.py
+++ invest.py
@@ -3,19 +3,20 @@ def control_function():
     choice =int(input('your choice:'))
     if choice == 0:
         amount = int(input('amount:'))
-        rate = int(input('rate:'))
+        rate = float(input('rate:'))
         time = int(input('time(year):'))
         finally_amount = 0
-        finally_amount = amount*(1+rate/100)**time
-        print('finally amount is',finally_amount)
+        finally_amount = amount*(1+rate)**time
+        print('finally amount is {:.2f}'.format(finally_amount))
     elif choice == 1:
         amount = int(input('amount:'))
-        rate = int(input('rate:'))
+        rate = float(input('rate:'))
         time = int(input('time(year):'))
         print('amount:{}'.format(amount))
         for t in range(1,time+1):
-            amount = amount*(1+rate/100)
-            print('year {}: ${}'.format(t,amount))
+            amount = amount*(1+rate)
+            print('year {}: ${:.2f}'.format(t,amount))
     else:
         print('invaild input')
+        control_function()
 control_function()    
