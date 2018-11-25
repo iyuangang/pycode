e3740cf2726482afa2094a142497aeada89e9f6e
Pythonun <87072009@qq.com>
2018-11-25 18:35:39 +0800

    bug fix

--- bos.py
+++ bos.py
@@ -21,6 +21,7 @@ def start_game():
     your_money = 1000
     while your_money > 0:
         print('<<<<<<< GAME STARTS! >>>>>>>')
+        print('You have {} now'.format(your_money))
         choices = ['Big','Small']
         your_choice = input('Big or Small:')
         if your_choice in choices:
@@ -31,6 +32,7 @@ def start_game():
             if youWin:
                 print('The points is',points,'You win !')
                 print('You gained {},you have {} now'.format(your_bet,your_money + your_bet))
+                your_money = your_money + your_bet
             else:
                 print('The pionts is',points,'You lose !')
                 print('You lost {},you have {} now'.format(your_bet,your_money - your_bet))
